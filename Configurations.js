5b8088512e582433e37db4a1d45ee4306323c800
RobaireTH <153534007+RobaireTH@users.noreply.github.com>
2024-02-24 00:23:05 +0100

    Update Configurations.js

--- Configurations.js
+++ Configurations.js
@@ -2,16 +2,16 @@ require("dotenv").config();
 
 let gg = process.env.MODS;
 if (!gg) {
-  gg = "918101187835";   // You can replace this number with yours //
+  gg = "2348106174312";   // You can replace this number with yours //
 }
 
 
 global.owner = gg.split(",");
 global.mongodb = process.env.MONGODB || "Put your alternative mongodb url here";
 global.sessionId = process.env.SESSION_ID || "ok";
-global.prefa = process.env.PREFIX || "-";
+global.prefa = process.env.PREFIX || "!,.#";
 global.tenorApiKey = process.env.TENOR_API_KEY || "AIzaSyCyouca1_KKy4W_MG1xsPzuku5oa8W358c";
-global.packname = process.env.PACKNAME || `Atlas Bot`;
+global.packname = process.env.PACKNAME || `Robaire`;
 global.author = process.env.AUTHOR || "by: Team Atlas";
 global.port = process.env.PORT || "10000";
 global.openAiAPI = process.env.OPENAI_API || "Put your openai API key here";
